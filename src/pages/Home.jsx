b145b0286c5094ec5b60624ab8940911c719358c
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-02-26 20:26:59 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,16 @@ import {Link} from '../Link.jsx'
 
 function HomePage() {
     return (
-    <>  
+    <div>  
       <img src="https://unavatar.io/pablotutormoegle" className="userIMG" alt="PabloIMG" />
       <h1>Pablo's Page</h1>
       <div className='linkcontainer'>
         <Link to={'/about'} className='linkbutton' > About </Link>
-        <br/>
         <Link to={'/projects'} className='linkbutton' > Projects </Link>
-        <br/>
         <Link to={'/hobbies'} className='linkbutton' > Hobbies/Personal Me </Link>
-        <br/>
         <Link to={'/3dprinting'} className='linkbutton' > 3D Printing </Link>
       </div>
-    </>
+    </div>
     )
 }
 
